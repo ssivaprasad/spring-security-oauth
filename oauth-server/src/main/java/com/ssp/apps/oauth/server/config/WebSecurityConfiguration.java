76054dc275278d77254b2d4cd37d2a0d2a93b23a
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-10 20:32:35 +0530

    Add Oauth Configuration

---
+++ oauth-server/src/main/java/com/ssp/apps/oauth/server/config/WebSecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.ssp.apps.oauth.server.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+    @Bean
+    public AuthenticationManager getAuthenticationManagerBean() throws Exception {
+        return super.authenticationManager();
+    }
+}
