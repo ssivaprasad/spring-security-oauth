95e007da870eb1ffb78105ae1b9c7bff7c072214
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-11 18:55:34 +0530

    Add Security Configuration

---
+++ resource-client/src/main/java/com/ssp/apps/oauth/resource/client/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.ssp.apps.oauth.resource.client.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/webjars/**", "/css/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.authorizeRequests().anyRequest().permitAll();
+        // @formatter:on
+    }
+
+
+}
