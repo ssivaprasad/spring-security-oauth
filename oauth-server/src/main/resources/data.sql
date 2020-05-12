8bccd5dfd9ac22b4af101c182471b55c93ac5f65
sivaprasads <somarouthu.sivaprasads@gmail.com>
2020-05-12 10:54:16 +0530

    Add client_credentials grant_type

--- oauth-server/src/main/resources/data.sql
+++ oauth-server/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity,  refresh_token_validity,
  resource_ids,  authorized_grant_types, additional_information) VALUES ('client_1', 'pass', 'http://localhost:8080/home', 'READ,WRITE', '3600', '10000', 
- 'inventory,payment', 'authorization_code,password,refresh_token,implicit', '{}');
+ 'inventory,payment', 'authorization_code,password,refresh_token,implicit,client_credentials', '{}');
  INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity,  refresh_token_validity,
  resource_ids,  authorized_grant_types, additional_information) VALUES ('client_2', 'pass', 'http://localhost:8080/home', 'READ,WRITE', '3600', '10000', 
  'inventory,payment', 'authorization_code,password,refresh_token,implicit', '{}');
